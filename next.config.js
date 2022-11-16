0e4d30e72fff67c30219f4ec47051c5c5a8baaf5
Abdul-Hafeez-Galib <galibabdulhafeez@gmail.com>
2022-11-17 01:30:07 +0530

    redis implemented for chat

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com', 'https://freesvg.org/img/winkboy.png'],
+  },
+  experimental: {
+    appDir: true
+  }
+}
